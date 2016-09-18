b6503534ae0c0c3b461de171d17c2c68f5a4f355
amee13bit <ameerali367@gmail.com>
2016-09-18 16:02:13 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('argentina',function(req,res){
+app.get('/argentina',function(req,res){
     res.send('ARGENTINA FOOTBALL');
 });
-app.get('messi',function(req,res){
+app.get('/messi',function(req,res){
     res.send('messi THE LEGEND');
 });
-app.get('DYBALA',function(req,res){
+app.get('/DYBALA',function(req,res){
     res.send('FEATURE OF ARGENTINA');
 });
 
